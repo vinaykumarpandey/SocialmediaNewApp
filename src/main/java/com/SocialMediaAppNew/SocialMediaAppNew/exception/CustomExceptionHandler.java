deae3a64a5feffc872f0a4c11caf1d137085e70f
vinaykumarpandey <vinaypandey.mit12@gmail.com>
2024-03-22 12:09:14 +0530

    Enhanced post api with validation constraints

--- src/main/java/com/SocialMediaAppNew/SocialMediaAppNew/exception/CustomExceptionHandler.java
+++ src/main/java/com/SocialMediaAppNew/SocialMediaAppNew/exception/CustomExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.SocialMediaAppNew.SocialMediaAppNew.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -38,6 +42,17 @@ public class CustomExceptionHandler extends  ResponseEntityExceptionHandler{
 
         return  new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
 
+    }
+
+   @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ErrorDetails errorDetails = new ErrorDetails(
+                      ex.getFieldError().getDefaultMessage() ,
+                      request.getDescription(true),
+                LocalDateTime.now());
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
 
